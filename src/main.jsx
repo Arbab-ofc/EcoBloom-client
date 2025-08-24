492fa282f1f17e8569590ae08a99879811dd34ce
arbab arshad <arshadsarhan786@gmail.com>
2025-08-25 00:55:11 +0530

    Removed HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { HashRouter } from "react-router-dom";
+
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-       <HashRouter>
+       
           <AuthProvider>
              <App />
           </AuthProvider>
-       </HashRouter>
+       
 )
